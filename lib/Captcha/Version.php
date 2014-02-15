d80bc1646f8c275853c5b2464f408dd1b0452a3e
Craig Heydenburg <craigh@mac.com>
2014-02-15 09:28:00 -0500

    raise core_max

--- lib/Captcha/Version.php
+++ lib/Captcha/Version.php
@@ -23,7 +23,7 @@ class Captcha_Version extends Zikula_AbstractVersion
         $meta['securityschema'] = array(
             'Captcha::'      => '::');
         $meta['core_min']       = '1.3.0'; // requires minimum 1.3.x only
-        $meta['core_max'] = '1.3.99'; // doesn't work with versions later than 1.3.99
+        $meta['core_max'] = '1.4.99';
         
         $meta['capabilities'] = array();
         $meta['capabilities'][HookUtil::PROVIDER_CAPABLE] = array('enabled' => true);
